b436b61deb863a6a50390ed4d62a7775b941fa87
zhang2055318 <huilin199851@foxmail.com>
2025-03-07 15:43:01 +0900

    Delete pipeline_settings directory

--- pipeline_settings/index.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    # TODO implement
-    print(event)
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello Lambda!')
-    }
